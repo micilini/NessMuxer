081e8fe2c597746889f8ca7be86bcea85f161718
Micilini Roll <micilini@hotmail.com>
2026-04-13 12:16:30 -0300

    n148: reduce size with tuned quant deadzone and coefficient tail pruning

--- src/encoder/n148/n148_quant.h
+++ src/encoder/n148/n148_quant.h
@@ -1,10 +1,12 @@
-#ifndef NESS_N148_QUANT_H
-#define NESS_N148_QUANT_H
+#ifndef N148_QUANT_H
+#define N148_QUANT_H
 
 #include <stdint.h>
 
 int n148_quant_qstep_from_qp(int qp);
 int n148_quantize_4x4(const int16_t* coeff, int16_t* out_zigzag, int qp);
+int n148_quantize_4x4_tuned(const int16_t* coeff, int16_t* out_zigzag, int qp, int is_intra, int is_chroma);
 void n148_quant_unscan_zigzag_4x4(const int16_t* zigzag, int16_t* natural);
+void n148_dequant_4x4(const int16_t* qcoeff, int16_t* out_coeff, int qp);
 
 #endif
